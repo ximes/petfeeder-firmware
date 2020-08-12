3e83f7cce699361cfc0a49e9eadff0b1f7951cb7
Stefano Franzin <stefano.franzin@gmail.com>
2020-08-12 20:47:00 +0200

    Initial Scaffold

---
+++ api/config/prod.secret.exs
@@ -0,0 +1,25 @@
+# In this file, we load production configuration and secrets
+# from environment variables. You can also hardcode secrets,
+# although such is generally not recommended and you have to
+# remember to add this file to your .gitignore.
+use Mix.Config
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") || Application.get_env(:api, ApiWeb.Endpoint )[:secret_key_base]
+
+config :api, ApiWeb.Endpoint,
+  http: [
+    port: String.to_integer(System.get_env("PORT") || "4000"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  secret_key_base: secret_key_base
+
+# ## Using releases (Elixir v1.9+)
+#
+# If you are doing OTP releases, you need to instruct Phoenix
+# to start each relevant endpoint:
+#
+#     config :api, ApiWeb.Endpoint, server: true
+#
+# Then you can assemble a release by calling `mix release`.
+# See `mix help release` for more information.
---
+++ firmware/config/config.exs
@@ -0,0 +1,65 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+import Config
+
+config :firmware, target: Mix.target()
+
+# Customize non-Elixir parts of the firmware. See
+# https://hexdocs.pm/nerves/advanced-configuration.html for details.
+
+config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
+
+# Set the SOURCE_DATE_EPOCH date for reproducible builds.
+# See https://reproducible-builds.org/docs/source-date-epoch/ for more information
+
+config :nerves, source_date_epoch: "1597087487"
+
+# Use Ringlogger as the logger backend and remove :console.
+# See https://hexdocs.pm/ring_logger/readme.html for more information on
+# configuring ring_logger.
+
+config :logger, backends: [RingLogger]
+
+if Mix.target() != :host do
+  import_config "target.exs"
+end
+
+# When we deploy to a device, we use the "prod" configuration:
+import_config "../../api/config/config.exs"
+import_config "../../api/config/prod.exs"
+
+config :api, ApiWeb.Endpoint,
+  # Nerves root filesystem is read-only, so disable the code reloader
+  code_reloader: false,
+  http: [port: 80],
+  # Use compile-time Mix config instead of runtime environment variables
+  load_from_system_env: false,
+  # Start the server since we're running in a release instead of through `mix`
+  server: true,
+  url: [host: "nerves.local", port: 80]
+
+config :vintage_net,
+  config: [
+    {"wlan0",
+      %{
+        type: VintageNetWiFi,
+        vintage_net_wifi: %{
+          networks: [
+            %{
+              key_mgmt: :wpa_psk,
+              psk: System.get_env("NERVES_NETWORK_PSK"),
+              ssid: System.get_env("NERVES_NETWORK_SSID")
+            }
+          ]
+        },
+        ipv4: %{method: :dhcp}
+      }
+    }
+  ]
+
+config :shoehorn,
+  app: :firmware,
+  init: [:nerves_runtime, :nerves_pack, :nerves_firmware_ssh]
